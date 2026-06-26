6a2dcb2d5835f18fb87e042485aaf62607e979bd
nishimura-yui <nishimura.yui@nxt-sys.jp>
2026-06-26 10:34:56 +0900

    Userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :birth_date, presence: true
 end
