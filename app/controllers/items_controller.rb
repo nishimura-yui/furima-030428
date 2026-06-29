937680afa61b1ee2b2e5e47ef48e0e0b96894f9e
nishimura-yui <nishimura.yui@nxt-sys.jp>
2026-06-29 12:04:56 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only:[:new, :create]
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shopping_fee_id, :prefecture_id, :shopping_day_id, :image)
   end
 end
---
+++ app/javascript/item_price.js
@@ -0,0 +1,20 @@
+document.addEventListener('turbo:load',() => {
+  const priceInput = document.getElementById('item-price');
+  if (!priceInput) return;
+  priceInput.addEventListener('input',() =>{
+    const price = parseInt(priceInput.value,10);
+    const taxDom = document.getElementById('add-tax-price');
+    const profitDom = document.getElementById('profit');
+    if(isNaN(price)){
+      taxDom.textContent = '';
+      profitDom.textContent ='';
+      return;
+    }
+
+    const tax = Math.floor(price * 0.1);
+    const profit = price - tax;
+
+    taxDom.textContent = tax;
+    profitDom.textContent = profit;
+  })
+})
