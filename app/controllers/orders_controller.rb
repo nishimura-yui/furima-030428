408e579a2d1c1690d639487ab516df3f0e870b7b
nishimura-yui <nishimura.yui@nxt-sys.jp>
2026-07-01 17:10:55 +0900

    Formオブジェクトを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,8 @@ class OrdersController < ApplicationController
   end
   
   def create
-    Order.create(user: current_user, item: @item)
+    @order = Order.create(order_params)
+    Address.create(address_params)
     redirect_to root_path
   end 
 
@@ -19,5 +20,14 @@ class OrdersController < ApplicationController
 
   def move_to_index
     redirect_to root_path if current_user == @item.user || @item.order.present?
+  end
+
+  def order_params
+    params.merge(user_id: current_user.id)
+    params.merge(item_id: @item.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture, :city, :address, :building_name, :phone_number).merge(order_id: @order.id)
   end
 end
