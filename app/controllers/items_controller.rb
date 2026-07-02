330cf327a418da2ffd951158b36661c2041a8baa
nishimura-yui <nishimura.yui@nxt-sys.jp>
2026-07-02 10:55:48 +0900

    売却済みの制御を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,6 +50,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user == @item.user
+    redirect_to root_path if current_user != @item.user || @item.order.present?
   end
 end
